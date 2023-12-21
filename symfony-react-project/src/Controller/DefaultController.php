2feceac0057f02e8d6d1b231298a74f162114da7
Leon Avetisyan <l.avetisyan@andersenlab.com>
2023-12-21 17:57:30 +0400

    new route and sample data were added in the default controller

--- symfony-react-project/src/Controller/DefaultController.php
+++ symfony-react-project/src/Controller/DefaultController.php
@@ -15,4 +15,49 @@ class DefaultController extends AbstractController
             'controller_name' => 'DefaultController',
         ]);
     }
+
+    #[Route('/api/users', name: 'users')]
+    public function getUsers()
+    {
+        $users = [
+            [
+                'id' => 1,
+                'name' => 'Olususi Oluyemi',
+                'description' => 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, ...',
+                'imageURL' => 'https://randomuser.me/api/portraits/women/50.jpg',
+            ],
+            [
+                'id' => 2,
+                'name' => 'Camila Terry',
+                'description' => 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, ...',
+                'imageURL' => 'https://randomuser.me/api/portraits/women/42.jpg',
+            ],
+            [
+                'id' => 3,
+                'name' => 'Joel Williamson',
+                'description' => 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, ...',
+                'imageURL' => 'https://randomuser.me/api/portraits/women/67.jpg',
+            ],
+            [
+                'id' => 4,
+                'name' => 'Deann Payne',
+                'description' => 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, ...',
+                'imageURL' => 'https://randomuser.me/api/portraits/women/50.jpg',
+            ],
+            [
+                'id' => 5,
+                'name' => 'Deann Payne',
+                'description' => 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, ...',
+                'imageURL' => 'https://randomuser.me/api/portraits/women/89.jpg',
+            ],
+        ];
+
+        $response = new Response();
+
+        $response->headers->set('Content-Type', 'application/json');
+        $response->headers->set('Access-Control-Allow-Origin', '*');
+        $response->setContent(json_encode($users));
+
+        return $response;
+    }
 }
