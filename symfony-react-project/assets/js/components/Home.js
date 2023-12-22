45431874249aef8577bf21610de2ea4bb62386a6
Leon Avetisyan <l.avetisyan@andersenlab.com>
2023-12-22 15:59:56 +0400

    Project's home page's markup was added

---
+++ symfony-react-project/assets/js/components/Home.js
@@ -0,0 +1,35 @@
+import { Component } from "react";
+import { BrowserRouter as Router, Link, Routes, Route, Navigate } from "react-router-dom";
+import Users from "./Users";
+import Posts from "./Posts";
+
+class Home extends Component {
+    render() {
+        return (
+            <div>
+                <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
+                    <Link className={"navbar-brand"} to={"/"}> Symfony React Project </Link>
+                    <div className="collapse navbar-collapse" id="navbarText">
+                        <ul className="navbar-nav mr-auto">
+                            <li className="nav-item">
+                                <Link className={"nav-link"} to={"/posts"}>Posts</Link>
+                            </li>
+                            <li className="nav-item">
+                                <Link className={"nav-link"} to={"/users"}>Users</Link>
+                            </li>
+                        </ul>
+                    </div>
+                </nav>
+                <Router>
+                    <Routes>
+                        <Route path="/" element={<Navigate to={"/users"} replace />} />
+                        <Route path="/users" element={Users} />
+                        <Route path="/posts" element={Posts} />
+                    </Routes>
+                </Router>
+            </div>
+        );
+    }
+}
+
+export default Home;
