9a6bd1c9bd58ddbab419ca326f4b104da43e8974
Leon Avetisyan <l.avetisyan@andersenlab.com>
2023-12-29 11:10:22 +0400

    app.js was edited

--- symfony-react-project/assets/js/app.js
+++ symfony-react-project/assets/js/app.js
@@ -4,9 +4,14 @@
  * We recommend including the built version of this JavaScript file
  * (and its CSS file) in your base layout (base.html.twig).
  */
-
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { BrowserRouter as Router } from 'react-router-dom';
 // any CSS you import will output into a single css file (app.css in this case)
 import '../styles/app.css';
 
 // start the Stimulus application
 import '../bootstrap';
+import Home from './components/Home';
+
+ReactDOM.render(<Router><Home/></Router>, document.getElementById('root'));
